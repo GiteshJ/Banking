da9dfbfe48cf5a37cf202d24f36a8f311a7c5777
Gitesh Jain <giteshjain844@gmail.com>
2021-02-17 17:19:22 +0530

    caching using redis cache

---
+++ src/main/java/com/banking/aspect/advice/CacheAdvice.java
@@ -0,0 +1,56 @@
+package com.banking.aspect.advice;
+
+import java.util.List;
+import java.util.Map;
+
+import java.lang.reflect.Method;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.banking.aspect.customAnnotation.Cache;
+import com.banking.cache.repository.CacheRepository;
+import com.banking.dto.CustomerDetailsDto;
+
+@Aspect
+@Component("CacheAdvice")
+public class CacheAdvice {
+	@Autowired
+    CacheRepository cacheRepository;
+
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Pointcut("@annotation(com.banking.aspect.customAnnotation.Cache)")
+    public void cacheMethod() { }
+
+    @Around(value = "cacheMethod()")
+    public Object Cache(ProceedingJoinPoint joinPoint) throws Throwable{
+        Method method = getCurrentMethod(joinPoint);
+        String key = method.getName();
+        logger.info("Cache ----"+key);
+
+        Object result = cacheRepository.findAll(key);
+        List<CustomerDetailsDto> resultMap = (List<CustomerDetailsDto>)result;
+        if(result!=null && resultMap.size()>0)
+            return resultMap;
+
+        result = joinPoint.proceed();
+        Cache cache = method.getAnnotation(Cache.class);
+        logger.info("Caching Start----"+key);
+        cacheRepository.save(key,(List<CustomerDetailsDto>) result,cache.ttl());
+        logger.info("Caching Done");
+        return result;
+    }
+
+    private java.lang.reflect.Method getCurrentMethod(JoinPoint joinPoint){
+        MethodSignature signature = (MethodSignature)  joinPoint.getSignature();
+        return signature.getMethod();
+    }
+}
---
+++ src/main/java/com/banking/cache/repository/CacheRepository.java
@@ -0,0 +1,37 @@
+package com.banking.cache.repository;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Repository;
+
+import com.banking.dto.CustomerDetailsDto;
+
+@Repository
+@SuppressWarnings("all")
+public class CacheRepository {
+
+    private ValueOperations valueoperations;
+    private RedisTemplate redisTemplate;
+
+    public CacheRepository(RedisTemplate redisTemplate) {
+        this.valueoperations=redisTemplate.opsForValue();
+        this.redisTemplate = redisTemplate;
+    }
+
+
+    public void save(String key,List<CustomerDetailsDto> map,int ttl) {
+    	this.valueoperations.set(key, map);
+        redisTemplate.expire(key,ttl,TimeUnit.HOURS);
+    }
+
+
+    public List<CustomerDetailsDto> findAll(String key){
+        System.out.println();
+        return (List<CustomerDetailsDto>) valueoperations.get(key);
+    }
+
+}
